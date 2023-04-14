0a5169c89e68841b39f3c19ccbf79638d9f4e6dd
rs-duyvd <duyvd@runsystem.net>
2023-04-14 16:15:51 +0700

    feat: crawl menu from grab and shopee

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  return res.status(200).end("hello");
+});
+
 app.get("/get-menus", async (req, res) => {
   const url = req.query.url;
-  console.log("🚀 ~ file: server.js:6 ~ app.get ~ url:", url)
+  console.log("🚀 ~ file: server.js:6 ~ app.get ~ url:", url);
   const crawl = require("./crawl.js");
-  let menus = []
+  let menus = [];
   try {
     if (url.includes("grab")) {
       menus = await crawl.getDataGrab(url);
     } else if (url.includes("shopeefood")) {
-      menus = await crawl.getDataShopeeFood(url)
+      menus = await crawl.getDataShopeeFood(url);
     }
   } catch (error) {
     console.log(error);
   }
-  
 
   return res
-      .status(200)
-      .set({ "content-type": "application/json; charset=utf-8" })
-      .end(JSON.stringify(menus));
+    .status(200)
+    .set({ "content-type": "application/json; charset=utf-8" })
+    .end(JSON.stringify(menus));
 });
 
 const server = app.listen(8081, function () {
